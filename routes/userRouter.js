b872d7ad23cb7de3e6550f5fe7fbcae2d186a546
anjana <anjanaslt0416@gmail.com>
2025-11-01 18:39:06 +0530

    9

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -1,70 +1,13 @@
-/*import express from "express";
-import { createUser,  loginUser,   } from "../controllers/userController.js";
-
-const userRouter = express.Router();
-
-userRouter.post("/", createUser)
-userRouter.post("/login",loginUser)
-
-
-
-
-export default userRouter*/
-
-/*import express from "express";
-import { checkAccountExists, createUser, loginOrRegister, loginUser } from "../controllers/userController.js";
-import { getMyOrders, getOrderById } from "../controllers/orderController.js";
-
-
-const userRouter = express.Router();
-
-// User authentication routes
-userRouter.post("/", createUser);
-userRouter.post("/login", loginUser);
-
-userRouter.post("/login-or-register", loginOrRegister); // New route for checkout
-
-userRouter.post("/check-account", checkAccountExists);
-
-// My Orders routes (protected)
-userRouter.get("/my-orders",  getMyOrders);
-userRouter.get("/my-orders/:orderId",  getOrderById);
-
-export default userRouter;*/
-
+// routes/userRouter.js - FIXED VERSION (removes duplicate authenticateToken)
 import express from "express";
 import { checkAccountExists, createUser, loginOrRegister, loginUser } from "../controllers/userController.js";
 import { getMyOrders, getOrderById } from "../controllers/orderController.js";
-import jwt from "jsonwebtoken";
+import { authenticateToken } from "../middleware/auth.js"; // Import from middleware
 import dotenv from "dotenv";
 
 dotenv.config();
 
 const userRouter = express.Router();
-
-// Authentication middleware
-const authenticateToken = (req, res, next) => {
-    const authHeader = req.headers['authorization'];
-    const token = authHeader && authHeader.split(' ')[1]; // Bearer TOKEN
-
-    if (!token) {
-        return res.status(401).json({
-            success: false,
-            message: "Access token is required"
-        });
-    }
-
-    jwt.verify(token, process.env.SECRET, (err, user) => {
-        if (err) {
-            return res.status(403).json({
-                success: false,
-                message: "Invalid or expired token"
-            });
-        }
-        req.user = user;
-        next();
-    });
-};
 
 // User authentication routes (public)
 userRouter.post("/", createUser);
