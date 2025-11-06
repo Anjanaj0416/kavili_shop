6b6c8889e34f18d961c9ed6aed94a1d199542e61
anjana <anjanaslt0416@gmail.com>
2025-11-07 00:01:10 +0530

    27

--- routes/contactRouter.js
+++ routes/contactRouter.js
@@ -1,13 +1,14 @@
 import express from "express";
 import { getContactInfo, updateContactInfo, initializeContactInfo } from "../controllers/contactController.js";
+import { adminAuth } from "../middleware/adminAuth.js";
 
 const contactRouter = express.Router();
 
 // Public route - get contact information
 contactRouter.get("/", getContactInfo);
 
-// Admin only routes
-contactRouter.put("/", updateContactInfo);
-contactRouter.post("/initialize", initializeContactInfo);
+// Admin only routes - PROTECTED with adminAuth middleware
+contactRouter.put("/", adminAuth, updateContactInfo);
+contactRouter.post("/initialize", adminAuth, initializeContactInfo);
 
 export default contactRouter;
